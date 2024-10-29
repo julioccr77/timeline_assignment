748ae05091334a0440ee1a037d5683cb7e205f8e
Julio <jcamargo@lean-tech.io>
2024-10-28 21:49:23 -0500

    clear code

--- src/components/ZoomControls/ZoomControls.js
+++ src/components/ZoomControls/ZoomControls.js
@@ -1,24 +1,34 @@
 import './ZoomControls.css';
 
 import React from 'react';
-import zoomInIcon from "../../assets/zoomIn.svg"
-import zoomOutIcon from "../../assets/zoomOut.svg"
+import zoomInIcon from "../../assets/zoomIn.svg";
+import zoomOutIcon from "../../assets/zoomOut.svg";
 
-const ZoomControls = ({zoomOut=()=>{}, zoomIn = ()=>{}}) => {
-
+const ZoomControls = ({ zoomOut = () => {}, zoomIn = () => {} }) => {
     return (
         <div className="zoom-controls">
-        <div>
-            <img src={zoomOutIcon} alt="zoomOut" width="40" height="40" onClick={zoomOut}/>
-            <p>Zoom out</p>
+            <div>
+                <img 
+                    src={zoomOutIcon} 
+                    alt="Zoom Out" 
+                    width="40" 
+                    height="40" 
+                    onClick={zoomOut} 
+                />
+                <p>Zoom out</p>
+            </div>
+            <div>
+                <img 
+                    src={zoomInIcon} 
+                    alt="Zoom In" 
+                    width="40" 
+                    height="40" 
+                    onClick={zoomIn} 
+                />
+                <p>Zoom in</p>
+            </div>
         </div>
-       <div>
-        <img src={zoomInIcon } alt="zoomIn" width="40" height="40" onClick={zoomIn} />
-        <p>Zoom in</p>
-       </div>
-    </div>
-    )
-
-}
+    );
+};
 
 export default ZoomControls;
